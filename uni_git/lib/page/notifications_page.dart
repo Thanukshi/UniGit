7e0a7b1734a90dfec2610afd0c908ad821313e53
Kothalawalage Eesha Thanukshi Wickramasinghe <thanukshi1234@gmail.com>
2022-04-08 15:14:07 +0530

    IT18202632

--- uni_git/lib/page/notifications_page.dart
+++ uni_git/lib/page/notifications_page.dart
@@ -19,5 +19,67 @@ class NotificationsPage extends StatelessWidget {
           centerTitle: true,
           backgroundColor: const Color(0xFF41A58D),
         ),
+        body: getBody(),
       );
+
+  Widget getBody() {
+    return ListView.builder(itemBuilder: (context, index) {
+      index = index + 1;
+      String indexNo = index.toString();
+      return getCard(indexNo);
+    });
+  }
+
+  Widget getCard(String item) {
+    return Card(
+        elevation: 1.5,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListTile(
+            onTap: () {},
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.all(4),
+                  padding: const EdgeInsets.all(2.0),
+                  child: Text(item,
+                      style: const TextStyle(fontSize: 17),
+                      textAlign: TextAlign.left),
+                ),
+                const SizedBox(width: 15),
+                Container(
+                  margin: const EdgeInsets.all(4),
+                  padding: const EdgeInsets.all(2.0),
+                  child: const Text("Jerry was created the \n repository now.",
+                      style: TextStyle(fontSize: 15),
+                      textAlign: TextAlign.left),
+                ),
+                const SizedBox(width: 5),
+                Container(
+                  width: 20,
+                  height: 20,
+                  margin: const EdgeInsets.all(5.0),
+                  padding: const EdgeInsets.all(2.0),
+                  child: const Icon(
+                    Icons.search,
+                    color: Color(0xFF276955),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Container(
+                  width: 20,
+                  height: 20,
+                  margin: const EdgeInsets.all(2.0),
+                  padding: const EdgeInsets.all(3.0),
+                  child: const Icon(
+                    Icons.cancel_sharp,
+                    color: Colors.redAccent,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
 }
