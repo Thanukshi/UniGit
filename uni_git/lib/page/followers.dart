12ddacc868396c5272901f42e512b642c8b18bc5
Kothalawalage Eesha Thanukshi Wickramasinghe <thanukshi1234@gmail.com>
2022-04-08 14:54:40 +0530

    IT18202632

--- uni_git/lib/page/followers.dart
+++ uni_git/lib/page/followers.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uni_git/Components/Common/common.dart';
+import 'package:uni_git/Constatnt/constants.dart';
 import 'package:uni_git/widget/navigation_drawer_widget.dart';
+import 'package:http/http.dart' as http;
 
 class FollowersPage extends StatefulWidget {
   const FollowersPage({Key? key}) : super(key: key);
@@ -9,10 +15,58 @@ class FollowersPage extends StatefulWidget {
 }
 
 class _FollowersPageState extends State<FollowersPage> {
+  List users = [];
+  bool isLoading = false;
+  var ID;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUser();
+    getUserId().then((id) {
+      setState(() {
+        ID = id;
+      });
+    });
+  }
+
+  getUserId() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? id = prefs.getString('id');
+
+    // ignore: non_constant_identifier_names
+    var ID = id;
+    print(ID);
+
+    return ID;
+  }
+
+  fetchUser() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    var url = Uri.parse(CommonService.URL + "/user/other_users/$ID");
+    // var url = Uri.parse("https://randomuser.me/api/?results=50");
+    var response = await http.get(url);
+    print(response.body);
+    if (response.statusCode == 200) {
+      var items = json.decode(response.body)['UserDetails'];
+
+      setState(() {
+        users = items;
+        isLoading = false;
+      });
+    } else {
+      users = [];
+      isLoading = false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavigationDrawerWidget(),
+      drawer: const NavigationDrawerWidget(),
       appBar: AppBar(
         title: const Text(
           'Followers',
@@ -28,11 +82,68 @@ class _FollowersPageState extends State<FollowersPage> {
       body: getBody(),
     );
   }
-}
 
-Widget getBody() {
-  return ListView.builder(itemBuilder: (context, index) {
-    index = index + 1;
-    return Text("Index $index");
-  });
+  Widget getBody() {
+    // ignore: prefer_is_empty
+    if (users.contains(null) || users.length < 0 || isLoading) {
+      return const Center(
+          child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation<Color>(primary),
+      ));
+    }
+    return ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (context, index) {
+          return getCard(users[index]);
+        });
+  }
+
+  Widget getCard(item) {
+    var fullName = item['user_name'];
+    var email = item['user_email'];
+    var profileUrl = item['user_image'];
+    return Card(
+        elevation: 1.5,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: ListTile(
+            onTap: () {},
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  width: 60,
+                  height: 60,
+                  margin: const EdgeInsets.all(2.0),
+                  padding: const EdgeInsets.all(2.0),
+                  decoration: BoxDecoration(
+                      color: primary,
+                      borderRadius: BorderRadius.circular(60 / 2),
+                      image: DecorationImage(
+                          fit: BoxFit.cover, image: NetworkImage(profileUrl))),
+                ),
+                const SizedBox(width: 30),
+                Container(
+                  margin: const EdgeInsets.all(4),
+                  padding: const EdgeInsets.all(2.0),
+                  child: Text(fullName,
+                      style: const TextStyle(fontSize: 17),
+                      textAlign: TextAlign.left),
+                ),
+                const SizedBox(width: 10),
+                Container(
+                  width: 20,
+                  height: 20,
+                  margin: const EdgeInsets.all(2.0),
+                  padding: const EdgeInsets.all(3.0),
+                  child: const Icon(
+                    Icons.cancel_sharp,
+                    color: Colors.redAccent,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
 }
