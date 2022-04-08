8b977b80346305d6c2619a7fdd18ef6e2438f474
Kothalawalage Eesha Thanukshi Wickramasinghe <thanukshi1234@gmail.com>
2022-04-08 15:05:27 +0530

    IT18202632

--- uni_git/lib/page/profile.dart
+++ uni_git/lib/page/profile.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_vector_icons/flutter_vector_icons.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uni_git/Components/LoginScreen/login_screen.dart';
 import 'package:uni_git/widget/navigation_drawer_widget.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -43,7 +44,7 @@ class InitState extends State<ProfilePage> {
     var width = MediaQuery.of(context).size.width;
     var height = MediaQuery.of(context).size.height;
     return Scaffold(
-      drawer: NavigationDrawerWidget(),
+      drawer: const NavigationDrawerWidget(),
       appBar: AppBar(
         title: const Text(
           'My Profile',
@@ -202,10 +203,20 @@ class InitState extends State<ProfilePage> {
                               Positioned(
                                 top: 120,
                                 right: 20,
-                                child: Icon(
-                                  AntDesign.setting,
-                                  color: Colors.grey[700],
-                                  size: 30,
+                                child: FloatingActionButton(
+                                  onPressed: () {
+                                    clearSharedPreferences();
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (_) => const LoginScreen(),
+                                        ));
+                                  },
+                                  child: Icon(
+                                    AntDesign.logout,
+                                    color: Colors.grey[700],
+                                    size: 30,
+                                  ),
                                 ),
                               ),
                               Positioned(
@@ -291,277 +302,20 @@ class InitState extends State<ProfilePage> {
     );
   }
 
-  // Widget build(BuildContext context) {
-  //   double width = MediaQuery.of(context).size.width;
-  //   double height = MediaQuery.of(context).size.height;
-  //   return Stack(
-  //     fit: StackFit.expand,
-  //     children: [
-  //       Container(
-  //         decoration: const BoxDecoration(
-  //           gradient: LinearGradient(
-  //             colors: [Color(0xFF41A58D), Color(0xFF276955)],
-  //             begin: FractionalOffset.bottomCenter,
-  //             end: FractionalOffset.topCenter,
-  //           ),
-  //         ),
-  //       ),
-  //       Scaffold(
-  //         backgroundColor: Colors.transparent,
-  //         body: SingleChildScrollView(
-  //           physics: const BouncingScrollPhysics(),
-  //           child: Padding(
-  //             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 50),
-  //             child: Column(
-  //               children: [
-  //                 Row(
-  //                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //                   children: [
-  //                     IconButton(
-  //                       onPressed: onPressed,
-  //                       icon: const Icon(
-  //                         AntDesign.arrowleft,
-  //                         color: Colors.white,
-  //                       ),
-  //                     ),
-  //                     // Icon(
-  //                     //   AntDesign.arrowleft,
-  //                     //   color: Colors.white,
-  //                     // ),
-  //                     const Icon(
-  //                       AntDesign.logout,
-  //                       color: Colors.white,
-  //                     ),
-  //                   ],
-  //                 ),
-  //                 const SizedBox(
-  //                   height: 20,
-  //                 ),
-  //                 const Text(
-  //                   'My Profile',
-  //                   textAlign: TextAlign.center,
-  //                   style: TextStyle(
-  //                     color: Colors.white,
-  //                     fontSize: 34,
-  //                     fontFamily: 'Nisebuschgardens',
-  //                   ),
-  //                 ),
-  //                 const SizedBox(
-  //                   height: 22,
-  //                 ),
-  //                 Container(
-  //                   height: height * 0.50,
-  //                   child: LayoutBuilder(
-  //                     builder: (context, constraints) {
-  //                       double innerHeight = constraints.maxHeight;
-  //                       double innerWidth = constraints.maxWidth;
-  //                       return Stack(
-  //                         fit: StackFit.expand,
-  //                         children: [
-  //                           Positioned(
-  //                             bottom: 0,
-  //                             left: 0,
-  //                             right: 0,
-  //                             child: Container(
-  //                               height: innerHeight * 0.70,
-  //                               width: innerWidth,
-  //                               decoration: BoxDecoration(
-  //                                 borderRadius: BorderRadius.circular(30),
-  //                                 color: Colors.white,
-  //                               ),
-  //                               child: Column(
-  //                                 children: [
-  //                                   const SizedBox(
-  //                                     height: 80,
-  //                                   ),
-  //                                   Text(
-  //                                     "$userName",
-  //                                     style: const TextStyle(
-  //                                       color: Color.fromRGBO(39, 105, 171, 1),
-  //                                       fontFamily: 'Nunito',
-  //                                       fontSize: 32,
-  //                                     ),
-  //                                   ),
-  //                                   const SizedBox(
-  //                                     height: 5,
-  //                                   ),
-  //                                   Text(
-  //                                     "$userEmail",
-  //                                     style: const TextStyle(
-  //                                       color: Color.fromRGBO(39, 105, 171, 1),
-  //                                       fontFamily: 'Nunito',
-  //                                       fontSize: 20,
-  //                                     ),
-  //                                   ),
-  //                                   const SizedBox(
-  //                                     height: 15,
-  //                                   ),
-  //                                   Row(
-  //                                     mainAxisAlignment:
-  //                                         MainAxisAlignment.center,
-  //                                     children: [
-  //                                       Column(
-  //                                         children: [
-  //                                           Text(
-  //                                             'Repositories',
-  //                                             style: TextStyle(
-  //                                               color: Colors.grey[700],
-  //                                               fontFamily: 'Nunito',
-  //                                               fontSize: 25,
-  //                                             ),
-  //                                           ),
-  //                                           const Text(
-  //                                             '10',
-  //                                             style: TextStyle(
-  //                                               color: Color.fromRGBO(
-  //                                                   39, 105, 171, 1),
-  //                                               fontFamily: 'Nunito',
-  //                                               fontSize: 25,
-  //                                             ),
-  //                                           ),
-  //                                         ],
-  //                                       ),
-  //                                       Padding(
-  //                                         padding: const EdgeInsets.symmetric(
-  //                                           horizontal: 25,
-  //                                           vertical: 8,
-  //                                         ),
-  //                                         child: Container(
-  //                                           height: 50,
-  //                                           width: 3,
-  //                                           decoration: BoxDecoration(
-  //                                             borderRadius:
-  //                                                 BorderRadius.circular(100),
-  //                                             color: Colors.grey,
-  //                                           ),
-  //                                         ),
-  //                                       ),
-  //                                       Column(
-  //                                         children: [
-  //                                           Text(
-  //                                             'Followers',
-  //                                             style: TextStyle(
-  //                                               color: Colors.grey[700],
-  //                                               fontFamily: 'Nunito',
-  //                                               fontSize: 25,
-  //                                             ),
-  //                                           ),
-  //                                           const Text(
-  //                                             '1',
-  //                                             style: TextStyle(
-  //                                               color: Color.fromRGBO(
-  //                                                   39, 105, 171, 1),
-  //                                               fontFamily: 'Nunito',
-  //                                               fontSize: 25,
-  //                                             ),
-  //                                           ),
-  //                                           const SizedBox(
-  //                                             height: 5,
-  //                                           ),
-  //                                         ],
-  //                                       ),
-  //                                     ],
-  //                                   ),
-  //                                 ],
-  //                               ),
-  //                             ),
-  //                           ),
-  //                           Positioned(
-  //                             top: 120,
-  //                             right: 20,
-  //                             child: Icon(
-  //                               AntDesign.setting,
-  //                               color: Colors.grey[700],
-  //                               size: 30,
-  //                             ),
-  //                           ),
-  //                           Positioned(
-  //                             top: 10,
-  //                             left: 0,
-  //                             right: 0,
-  //                             child: Center(
-  //                               child: Container(
-  //                                 child: CircleAvatar(
-  //                                   radius: 88,
-  //                                   backgroundColor: Colors.grey,
-  //                                   backgroundImage: userImage.isNotEmpty
-  //                                       ? NetworkImage(userImage)
-  //                                       : null,
-  //                                 ),
-  //                               ),
-  //                             ),
-  //                           ),
-  //                         ],
-  //                       );
-  //                     },
-  //                   ),
-  //                 ),
-  //                 const SizedBox(
-  //                   height: 30,
-  //                 ),
-  //                 Container(
-  //                   height: height * 0.5,
-  //                   width: width,
-  //                   decoration: BoxDecoration(
-  //                     borderRadius: BorderRadius.circular(30),
-  //                     color: Colors.white,
-  //                   ),
-  //                   child: Padding(
-  //                     padding: const EdgeInsets.symmetric(horizontal: 15),
-  //                     child: Column(
-  //                       children: [
-  //                         const SizedBox(
-  //                           height: 20,
-  //                         ),
-  //                         const Text(
-  //                           'My Repositories',
-  //                           style: TextStyle(
-  //                             color: Color.fromRGBO(39, 105, 171, 1),
-  //                             fontSize: 27,
-  //                             fontFamily: 'Nunito',
-  //                           ),
-  //                         ),
-  //                         const Divider(
-  //                           thickness: 2.5,
-  //                         ),
-  //                         const SizedBox(
-  //                           height: 10,
-  //                         ),
-  //                         Container(
-  //                           height: height * 0.15,
-  //                           decoration: BoxDecoration(
-  //                             color: Colors.grey,
-  //                             borderRadius: BorderRadius.circular(30),
-  //                           ),
-  //                         ),
-  //                         const SizedBox(
-  //                           height: 10,
-  //                         ),
-  //                         Container(
-  //                           height: height * 0.15,
-  //                           decoration: BoxDecoration(
-  //                             color: Colors.grey,
-  //                             borderRadius: BorderRadius.circular(30),
-  //                           ),
-  //                         ),
-  //                       ],
-  //                     ),
-  //                   ),
-  //                 )
-  //               ],
-  //             ),
-  //           ),
-  //         ),
-  //       )
-  //     ],
-  //   );
-  // }
+  void clearSharedPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove("token");
+    await prefs.remove("id");
+    await prefs.remove("user_email");
+    await prefs.remove("user_name");
+    await prefs.remove("user_image");
+  }
 
   void onPressed() {
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => NavigationDrawerWidget(),
+        builder: (context) => const NavigationDrawerWidget(),
       ),
     );
   }
